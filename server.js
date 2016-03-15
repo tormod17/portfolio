f92de624b12b151c31c22a69e47c5ad9b5a5b683
tormod17 <tormodsmith@gmail.com>
2016-03-15 22:25:01 +0000

    adding file upload

--- server.js
+++ server.js
@@ -5,120 +5,158 @@ const server = new Hapi.Server();
 const port = process.env.PORT || 3000;
 
 const Inert = require('inert');
-const Path =require('path');
+const Path = require('path');
 const Vision = require('vision');
 const Handlebars = require('handlebars');
 
 const redis = require('./lib/redis.js');
 
+const Login = require('hapi-login');
+
+
+const Bcrypt = require('bcrypt');
+const Joi = require('joi');
+
+
+var custom_fields = require('./lib/custom_fields');
+
+const login_handler = require('./lib/login_handler.js');
+const addPost_handler = require('./lib/addPost_handler.js');
+const upload_handler = require('./lib/upload_handler');
+const uploadcare_handler =require('./lib/uploadcare_handler.js');
+
+
+
+var opts = {
+    fields: custom_fields,
+    handler: login_handler,
+    fail_action_handler: login_handler
+};
+
+
+var HapiLogin = {
+    register: Login,
+    options: opts
+}
+
 
 server.connection({
-	port:port	
+    port: port
 });
 
 const plugins = [
-		Inert,
-		Vision
+    Inert,
+    Vision,
+    HapiLogin
 ];
 
-server.register(plugins,(err) => {
+server.register(plugins, (err) => {
 
-		if(err) console.log(err);
+            if (err) console.log(err);
 
-		server.views({
-			engines:{html:Handlebars},
-			relativeTo:__dirname,
-			path:'public'
+            server.views({
+                engines: { html: Handlebars },
+                relativeTo: __dirname,
+                path: 'public'
 
 
-		});
+            });
 
-		server.route([{
-				method:'GET',
-				path:'/',
-				handler:(request, reply) => {
-					 
-						  
-						   redis.getAllHashes('sortedPosts',(postsArr) => {
-                          	   		console.log('GAH', postsArr.length);
-	                          	    var allBlogs={};
-	                          		postsArr.reverse().map( (obj) => {
-	                          		 	obj.body= obj.body;
-	                          		 	var timeNdate =  new Date(Number(obj.date));
-	                          		 	obj.date = timeNdate;
+            server.route([{
+                        method: 'GET',
+                        path: '/',
+                        handler: (request, reply) => {
 
-	                          		});
 
-		                          		allBlogs.blogs = postsArr;
-		                          		allBlogs.title ='Work In Progress, Learning Blog'
-	                          	    reply.view('index', allBlogs);
+                            redis.getAllHashes('sortedPosts', (postsArr) => {
+                                console.log('GAH', postsArr.length);
+                                var allBlogs = {};
+                                postsArr.reverse().map((obj) => {
+                                    obj.body = obj.body;
+                                    var timeNdate = new Date(Number(obj.date));
+                                    obj.date = timeNdate;
 
-                          	                          	    	
-                          	    
+                                });
+                                allBlogs.blogs = postsArr;
+                                allBlogs.title = 'Work In Progress, Learning Blog'
+                                reply.view('index', allBlogs);
 
-	                        
-						  });
-                      		
-                    		
-						}
-					  
-				},
-				{
-					method:'GET',
-					path:'/assets/{params*2}',
-					handler:{
-						directory: {
-							path:'assets/'
-							}						
-						}
-				},
-				{
-					method:'GET',
-					path:'/assets/{params*3}',
-					handler:{
-						directory: {
-							path:'assets/'
-							}						
-						}
-				},
-				{
-					method:'GET',
-					path:'/assets/{params*4}',
-					handler:{
-						directory: {
-							path:'assets/'
-							}						
-						}
-				},
-				{
-					method :'GET',
-					path:'/blog',
-					handler:(request,reply) => {
-						reply.view('blog', {	
-											title:'Let\'s write',
-																				});
-						}
+                            });
+                        }
+                    }, {
+                        method: 'GET',
+                        path: '/assets/{params*}',
+                        handler: {
+                            directory: {
+                                path: 'assets/'
+                            }
+                        }
+                    },
+                    {
+                        method: 'GET',
+                        path: '/lib/uploads/{params*}',
+                        handler: {
+                            directory: {
+                                path: 'lib/uploads/'
+                            }
+                        }
+                    },
 
-				},
-				{
-					method:'POST',
-					path:'/blog',
-					handler: (request ,reply) => {
-							console.log('PAYLOAD', request.payload);
-							console.log('PAYLOAD', request.headers);
+                    {
+                        method: 'GET',
+                        path: '/blogLogin',
+                        handler: (request, reply) => {
+                            reply.view('login');
+                        }
 
-							redis.addtoRedis(request.payload);
-							reply.redirect('/blog');
-						}
-					
-				}
-		
-			
-		]);
+                    }, {
+                        method: 'POST',
+                        path: '/blogpage',
+                        handler: login_handler
 
-});
+                    }, {
+                        method: 'POST',
+                        path: '/redis',
+                        handler: addPost_handler
+                    }, {
+                        method: 'POST',
+                        path: '/redis/delete',
+                        handler: (request, reply) => {
+                            console.log('DEL', request.payload.post_name);
+                            redis.removePostFromList('sortedPosts', request.payload.post_name);
+                            redis.getFile('new-file');
+                            reply.view('blog', {
+                                title: 'Let\'s write',
+                                name: 'friend'
+                            });
+                        }
+                    },
+                    {
+                        method: 'POST',
+                        path: '/uploadcare',
+                        handler: uploadcare_handler
+                    },
 
-server.start(err => {
-	if (err) throw err;
-	console.log('Server is running at : ' + server.info.uri);
-});
+                    {
+
+                        method: 'POST',
+                        path: '/upload',
+                        config: {
+                            payload: {
+                                maxBytes: 209715200,
+                                output: 'stream',
+                                parse: true,
+                                allow:'multipart/form-data'
+                            	}
+                            },
+                        handler: upload_handler
+                    }
+
+                    ]);
+
+            });
+
+        server.start(err => {
+            if (err) throw err;
+            console.log('Server is running at : ' + server.info.uri);
+        });
